eb53a14494ea6289bb9c952db707352e6a463f0f
cosmos <Yy1226015813@163.com>
2024-08-29 16:30:54 +0800

    yuan

---
+++ element_land/五行大陆/Pet.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef _PET_H
+#define _PET_H
+#include <string>
+class Pet
+{
+public:
+	Pet();
+	Pet(std::string name,int life,
+		int power, int exp, int protectPower,
+		int level, int money);
+	~Pet();
+	std::string getName();
+	void setName(std::string name);
+	int getLife()const;
+	void setLife(int life);
+	int getPower()const;
+	void setPower(int power);
+	int getProtectPower()const;
+	void setProtectPower(int protectPower);
+	int getExp()const;
+	virtual void setExp(int exp);//设置经验
+	int getLevel()const;
+	void setLevel(int level);
+	int getMoney()const;
+	void setMoney(int money);
+	void showInfo();//打印属性
+	void initRevive(int life, int energy);//战斗致死，回到战斗前状态。
+private:
+	string name;
+	int life;//生命值
+	int power;//攻击力
+	int protectPower;//护甲
+	int exp;//经验
+	int level;//等级
+	int money;//金钱
+};
+#endif // !__PET_H__
---
+++ element_land/五行大陆/constTable.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef __CONSTTABLE_H__
+#define __CONSTTABLE_H__
+const int SHOPLISTCOUNT = 6;//武器店最大数目
+const std::string SHOPLIST[SHOPLISTCOUNT] = { "刀","剑","弓箭","铠甲","盾牌","鞋子" };//武器店东西
+//const std::string SHOPLIST[SHOPLISTCOUNT] = { "DAO","JIAN","GONGJIAN","KAIJIA","DUNPAI","XIEZI" };//武器店东西
+
+
+
+
+
+
+
+
+
+#endif // !__CONSTTABLE_H__
